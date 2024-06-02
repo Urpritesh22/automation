ae2432639153ed0a8a7d37444090c4d95ae92795
Pritesh Chopade <121218533+Urpritesh22@users.noreply.github.com>
2024-06-02 16:25:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-pritesh"
+    bucket = "mydev-tf-state-bucket-project-terraform-pritesh-1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table1"
