2e265716f6bd591cbfd212664461b4ec8187f401
Pritesh Chopade <121218533+Urpritesh22@users.noreply.github.com>
2024-06-02 17:42:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,26 +2,18 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Use existing VPC
-data "aws_vpc" "existing" {
-  id = "vpc-07ec39ca6a3c2d055"
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
 }
 
-# Retrieve the subnets in the existing VPC
-data "aws_subnet_ids" "existing" {
-  vpc_id = data.aws_vpc.existing.id
-}
-
-# Retrieve details of the first subnet
-data "aws_subnet" "first" {
-  id = data.aws_subnet_ids.existing.ids[0]
-}
-
-# Create security group with firewall rules
+#Create security group with firewall rules
 resource "aws_security_group" "jenkins-sg-2022" {
   name        = var.security_group
-  description = "security group for EC2 instance"
-  vpc_id      = data.aws_vpc.existing.id
+  description = "security group for Ec2 instance"
 
   ingress {
     from_port   = 8080
@@ -30,14 +22,14 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+ ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # outbound from Jenkins server
+ # outbound from jenkis server
   egress {
     from_port   = 0
     to_port     = 65535
@@ -45,18 +37,17 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
+  tags= {
     Name = var.security_group
   }
 }
 
 resource "aws_instance" "myFirstInstance" {
-  ami                      = var.ami_id
-  key_name                 = var.key_name
-  instance_type            = var.instance_type
-  vpc_security_group_ids   = [aws_security_group.jenkins-sg-2022.id]
-  subnet_id                = data.aws_subnet.first.id
-  tags = {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  tags= {
     Name = var.tag_name
   }
 }
@@ -65,11 +56,7 @@ resource "aws_instance" "myFirstInstance" {
 resource "aws_eip" "myFirstInstance" {
   vpc      = true
   instance = aws_instance.myFirstInstance.id
-  tags = {
+tags= {
     Name = "my_elastic_ip"
   }
-}
-
-output "subnet_ids" {
-  value = data.aws_subnet_ids.existing.ids
 }
